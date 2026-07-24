5d2ef6a69e409b1c1cc7858decb34d92e4a70239
JoaPeralta <peralta.joaquin@hotmail.com>
2026-07-24 18:16:50 -0300

    fix: avoid global Product Price precision change on Odoo 19

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,5 @@ from . import test_webhook_resource_security
 from . import test_webhook_notification_security
 from . import test_meli_login_no_token_exposure
 from . import test_http_backend_contract
+from . import test_no_global_price_precision
 from . import test_http_backend_selection
